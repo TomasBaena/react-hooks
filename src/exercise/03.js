e22758f7d9eb0479de2a72c0bd867d881135d7fb
Tomas Baena <tomas@lumaki.io>
2021-06-02 21:24:47 -0400

    3.1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -7,13 +7,19 @@
 // the state and a mechanism for updating the state down into the 
 // components that need it
 
+//credit 1
+//sometimes it's valid to push the state back down (colocate state)
+//move name state to it's own component and display only animal entry
+
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
+
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input id="name" value={name} onChange={event=> setName(event.target.value)} />
     </div>
   )
 }
@@ -32,22 +38,19 @@ function FavoriteAnimal({animal, onAnimalChange}) {
   )
 }
 
-// 🐨 uncomment this
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
-  // 🐨 add a useState for the animal
-  const [name, setName] = React.useState('')
   const [animal, setAnimal] = React.useState('')
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name />
       {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)} />
       {/* 🐨 pass the animal prop here */}
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
