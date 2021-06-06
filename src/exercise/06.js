c07019e75c24f64bddfbb9d6bcaba0532e03662c
Tomas Baena <tomas@lumaki.io>
2021-06-06 13:32:35 -0400

    6.4 error boundry

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -30,8 +30,25 @@
 
 // 6.2 using status organize code
 // 6.3 use object in state to reduce the amount of state setting and initializations
+// 6.4 error boundries https://reactjs.org/docs/error-boundaries.html
+
 import * as React from 'react'
 import {fetchPokemon, PokemonDataView, PokemonForm, PokemonInfoFallback} from '../pokemon'
+
+class ErrorBoundary extends React.Component {
+  state = {error: null}
+  static getDerivedStateFromError(error) {
+    return {error}
+  }
+  render() {
+    const {error} = this.state
+    if (error) {
+      return <this.props.FallbackComponent error={error} />
+    }
+
+    return this.props.children
+  }
+}
 
 function PokemonInfo({pokemonName}) {
   const [state, setState] = React.useState({
@@ -51,13 +68,16 @@ function PokemonInfo({pokemonName}) {
   if (status == 'idle') return 'Submit a pokemon'
   else if (status == 'pending') return <PokemonInfoFallback name={pokemonName} />
   else if (status == 'resolved') return <PokemonDataView pokemon={pokemon} />   
-  else if (status == 'rejected'){
-    return (
-      <div role="alert">
-        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-      </div>
-    )
-  }     
+  else if (status == 'rejected') throw error
+}
+
+function ErrorFallback({error}) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+    </div>
+  )
 }
 
 function App() {
@@ -73,7 +93,9 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
